57011def19af8b78b112c7739d0c16e430fbf280
MahmoudTonsy <m_tonsy@yahoo.com>
2024-04-17 08:02:08 +0200

    implementation InsertionSort Algorithm

---
+++ js/InsertionSort.js
@@ -0,0 +1,29 @@
+
+function insertionSort(arr){
+
+    for(var i = 1 ; i < arr.length ; i++){
+
+        var key = arr[i];
+        j = i - 1 ; 
+
+        while(j >= 0 && arr[j] > key ){
+
+             arr[j+1] = arr[j] 
+             j = j - 1 ;
+
+        }
+        arr[j+1] = key
+        
+
+    }
+
+    return arr
+}
+console.log(insertionSort([80, 90, 60 , 30, 50, 70]));
+
+// Quadratic Time complexity
+// Bestcase o(n)
+// Averagecase o(n^2)
+// Worstcase o(n^2)  
+
+
